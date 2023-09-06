ae64430f1bc0aae52c7db2a516335a4020e184d8
kashayap-7453 <neeraj74530@gmail.com>
2023-09-06 16:22:33 +0530

    add resume

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const btn = document.querySelector('#btn');
+// const btn = document.querySelector('#btn');
 
-const  hiddenVideo = document.querySelector('.hidden-video');
+// const  hiddenVideo = document.querySelector('.hidden-video');
 
-const close = document.querySelector('.cross')
+// const close = document.querySelector('.cross')
 
-btn.addEventListener('click',()=>{
-  hiddenVideo.classList.add('show');
-})
+// btn.addEventListener('click',()=>{
+//   hiddenVideo.classList.add('show');
+// })
 
-close.addEventListener('click',()=>{
-  hiddenVideo.classList.remove('show');
-})
+// close.addEventListener('click',()=>{
+//   hiddenVideo.classList.remove('show');
+// })
 
 
 
@@ -20,96 +20,96 @@ close.addEventListener('click',()=>{
 //     My services popup section  start 
 // =========================================
 
-//      My service 1st box start
-// --------------------------------
+// //      My service 1st box start
+// // --------------------------------
 
-document.querySelector(".box-my-services-parent").addEventListener('click' , function(){
-  document.querySelector(".services-hidden-box-parent").style.display = "block";
-})
+// document.querySelector(".box-my-services-parent").addEventListener('click' , function(){
+//   document.querySelector(".services-hidden-box-parent").style.display = "block";
+// })
 
-document.querySelector(".service-close").addEventListener('click' , function(){
-  document.querySelector(".services-hidden-box-parent").style.display = "none";
-})
+// document.querySelector(".service-close").addEventListener('click' , function(){
+//   document.querySelector(".services-hidden-box-parent").style.display = "none";
+// })
 
-//      My service 1st box end
-// ----------------------------------
-
-
-//      My service 2nd box start
-// --------------------------------
-
-document.querySelector(".box-a-parent").addEventListener('click' , function(){
-  document.querySelector(".services-second-popup").style.display = "block";
-})
-
-document.querySelector(".second-popup-close").addEventListener('click' , function(){
-  document.querySelector(".services-second-popup").style.display = "none";
-})
-
-//      My service 2nd box end
-// ----------------------------------
+// //      My service 1st box end
+// // ----------------------------------
 
 
-//      My service 3nd box start
-// --------------------------------
+// //      My service 2nd box start
+// // --------------------------------
 
-document.querySelector(".box-c-parent").addEventListener('click' , function(){
-  document.querySelector(".services-third-popup").style.display = "block";
-})
+// document.querySelector(".box-a-parent").addEventListener('click' , function(){
+//   document.querySelector(".services-second-popup").style.display = "block";
+// })
 
-document.querySelector(".close-third-popup").addEventListener('click' , function(){
-  document.querySelector(".services-third-popup").style.display = "none";
-})
+// document.querySelector(".second-popup-close").addEventListener('click' , function(){
+//   document.querySelector(".services-second-popup").style.display = "none";
+// })
 
-//      My service 3nd box end
-// ----------------------------------
-
-
-
-//      My service 4th box start
-// --------------------------------
-
-document.querySelector(".box-d-parent").addEventListener('click' , function(){
-  document.querySelector(".services-fourth-popup").style.display = "block";
-})
-
-document.querySelector(".close-fourth-popup").addEventListener('click' , function(){
-  document.querySelector(".services-fourth-popup").style.display = "none";
-})
+// //      My service 2nd box end
+// // ----------------------------------
 
 
-//      My service 4th box end
-// ----------------------------------
+// //      My service 3nd box start
+// // --------------------------------
+
+// document.querySelector(".box-c-parent").addEventListener('click' , function(){
+//   document.querySelector(".services-third-popup").style.display = "block";
+// })
+
+// document.querySelector(".close-third-popup").addEventListener('click' , function(){
+//   document.querySelector(".services-third-popup").style.display = "none";
+// })
+
+// //      My service 3nd box end
+// // ----------------------------------
 
 
 
-//      My service 5th box start
-// --------------------------------
+// //      My service 4th box start
+// // --------------------------------
 
-document.querySelector(".box-e-parent").addEventListener('click' , function(){
-  document.querySelector(".services-fifth-popup").style.display = "block";
-})
+// document.querySelector(".box-d-parent").addEventListener('click' , function(){
+//   document.querySelector(".services-fourth-popup").style.display = "block";
+// })
 
-document.querySelector(".fifth-popup-close").addEventListener('click' , function(){
-  document.querySelector(".services-fifth-popup").style.display = "none";
-})
-
-
-//      My service 5th box end
-// ----------------------------------
+// document.querySelector(".close-fourth-popup").addEventListener('click' , function(){
+//   document.querySelector(".services-fourth-popup").style.display = "none";
+// })
 
 
+// //      My service 4th box end
+// // ----------------------------------
 
-//      My service 6th box start
-// --------------------------------
 
-document.querySelector(".box-f-parent").addEventListener('click' , function(){
-  document.querySelector(".services-six-popup").style.display = "block";
-})
 
-document.querySelector(".six-popup-close").addEventListener('click' , function(){
-  document.querySelector(".services-six-popup").style.display = "none";
-})
+// //      My service 5th box start
+// // --------------------------------
+
+// document.querySelector(".box-e-parent").addEventListener('click' , function(){
+//   document.querySelector(".services-fifth-popup").style.display = "block";
+// })
+
+// document.querySelector(".fifth-popup-close").addEventListener('click' , function(){
+//   document.querySelector(".services-fifth-popup").style.display = "none";
+// })
+
+
+// //      My service 5th box end
+// // ----------------------------------
+
+
+
+// //      My service 6th box start
+// // --------------------------------
+
+// document.querySelector(".box-f-parent").addEventListener('click' , function(){
+//   document.querySelector(".services-six-popup").style.display = "block";
+// })
+
+// document.querySelector(".six-popup-close").addEventListener('click' , function(){
+//   document.querySelector(".services-six-popup").style.display = "none";
+// })
 
 
 //      My service 6th box end
@@ -145,81 +145,81 @@ moblieNav.addEventListener("click" , () => toggleNavbar());
 //     My blog section start
 // =========================================
 
-// box 1
-// -------
-document.querySelector("#blog-box-a").addEventListener("click", function(){
-  document.querySelector(".blog-hidden-a").style.display = "block";
-})
+// // box 1
+// // -------
+// document.querySelector("#blog-box-a").addEventListener("click", function(){
+//   document.querySelector(".blog-hidden-a").style.display = "block";
+// })
 
-document.querySelector(".blog-close-a").addEventListener("click" , function(){
-  document.querySelector(".blog-hidden-a").style.display = "none";
-})
+// document.querySelector(".blog-close-a").addEventListener("click" , function(){
+//   document.querySelector(".blog-hidden-a").style.display = "none";
+// })
 
-// box 2
-// -------
+// // box 2
+// // -------
 
-document.querySelector("#blog-box-b").addEventListener("click" , function(){
-  document.querySelector(".blog-hidden-b").style.display = "block";
-})
+// document.querySelector("#blog-box-b").addEventListener("click" , function(){
+//   document.querySelector(".blog-hidden-b").style.display = "block";
+// })
 
-document.querySelector(".blog-close-b").addEventListener("click" ,function(){
-  document.querySelector(".blog-hidden-b").style.display = "none";
-})
+// document.querySelector(".blog-close-b").addEventListener("click" ,function(){
+//   document.querySelector(".blog-hidden-b").style.display = "none";
+// })
 
-// box 3
-// -------
+// // box 3
+// // -------
 
-document.querySelector("#blog-box-c").addEventListener("click" , function(){
-  document.querySelector(".blog-hidden-c").style.display = "block";
-})
+// document.querySelector("#blog-box-c").addEventListener("click" , function(){
+//   document.querySelector(".blog-hidden-c").style.display = "block";
+// })
 
-document.querySelector(".blog-close-c").addEventListener("click" ,function(){
-  document.querySelector(".blog-hidden-c").style.display = "none";
-})
+// document.querySelector(".blog-close-c").addEventListener("click" ,function(){
+//   document.querySelector(".blog-hidden-c").style.display = "none";
+// })
 
-// box 4
-// -------
+// // box 4
+// // -------
 
-document.querySelector("#blog-box-d").addEventListener("click" , function(){
-  document.querySelector(".blog-hidden-d").style.display = "block";
-})
+// document.querySelector("#blog-box-d").addEventListener("click" , function(){
+//   document.querySelector(".blog-hidden-d").style.display = "block";
+// })
 
-document.querySelector(".blog-close-d").addEventListener("click" , function(){
-  document.querySelector(".blog-hidden-d").style.display =  "none";
-})
+// document.querySelector(".blog-close-d").addEventListener("click" , function(){
+//   document.querySelector(".blog-hidden-d").style.display =  "none";
+// })
 
-// box 5
-// -------
+// // box 5
+// // -------
 
-document.querySelector("#blog-box-e").addEventListener("click" , function(){
-  document.querySelector(".blog-hidden-e").style.display = "block";
-})
+// document.querySelector("#blog-box-e").addEventListener("click" , function(){
+//   document.querySelector(".blog-hidden-e").style.display = "block";
+// })
 
-document.querySelector(".blog-close-e").addEventListener("click" ,function(){
-  document.querySelector(".blog-hidden-e").style.display = "none";
-})
+// document.querySelector(".blog-close-e").addEventListener("click" ,function(){
+//   document.querySelector(".blog-hidden-e").style.display = "none";
+// })
 
-// box 6
-// -------
+// // box 6
+// // -------
 
-document.querySelector("#blog-box-f").addEventListener("click" , function(){
-  document.querySelector(".blog-hidden-f").style.display = "block";
-})
+// document.querySelector("#blog-box-f").addEventListener("click" , function(){
+//   document.querySelector(".blog-hidden-f").style.display = "block";
+// })
 
-document.querySelector(".blog-close-f").addEventListener("click" ,function(){
-  document.querySelector(".blog-hidden-f").style.display = "none";
-})
+// document.querySelector(".blog-close-f").addEventListener("click" ,function(){
+//   document.querySelector(".blog-hidden-f").style.display = "none";
+// })
 
-// box 7
-// -------
+// // box 7
+// // -------
 
-document.querySelector("#blog-box-g").addEventListener("click" , function(){
-  document.querySelector(".blog-hidden-g").style.display = "block";
-})
+// document.querySelector("#blog-box-g").addEventListener("click" , function(){
+//   document.querySelector(".blog-hidden-g").style.display = "block";
+// })
 
-document.querySelector(".blog-close-g").addEventListener("click" ,function(){
-  document.querySelector(".blog-hidden-g").style.display = "none";
-})
+// document.querySelector(".blog-close-g").addEventListener("click" ,function(){
+//   document.querySelector(".blog-hidden-g").style.display = "none";
+// })
 
 // =========================================
 //     My blog section end
